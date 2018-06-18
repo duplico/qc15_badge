d052709f4c712c37f397b830a48083d4c0eb870e
George Louthan <duplico@dupli.co>
2018-06-17 21:20:18 -0500

    Change clock frequency, break out the LED controller into a new file.

---
+++ ccs_workspace/qc15_main/ht16d35b.h
@@ -0,0 +1,15 @@
+/*
+ * ht16d35b.h
+ *
+ *  Created on: Jun 17, 2018
+ *      Author: george
+ */
+
+#ifndef HT16D35B_H_
+#define HT16D35B_H_
+
+void ht16d_init_io();
+void ht16d_init();
+void led_send_gray();
+
+#endif /* HT16D35B_H_ */
