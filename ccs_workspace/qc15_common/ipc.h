4609f83bea98297a1dc50b34d13b996b0b385f0e
George Louthan <duplico@dupli.co>
2018-07-08 15:10:09 -0500

    Create and link a shared qc15_common project for IPC and other config.

--- ccs_workspace/qc15_common/ipc.h
+++ ccs_workspace/qc15_common/ipc.h
@@ -1,14 +1,13 @@
 /*
  * ipc.h
  *
- *  Created on: Jun 17, 2018
+ *  Created on: Jul 7, 2018
  *      Author: george
  */
 
 #ifndef IPC_H_
 #define IPC_H_
 
-void ipc_init_io();
 void ipc_init();
 
 #endif /* IPC_H_ */
