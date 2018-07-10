a5cd409ddabfc447c1b5d97524f1804b6a5bbd0c
George Louthan <duplico@dupli.co>
2018-07-09 21:10:28 -0500

    Do a super stripped-down test of the UARTs.

--- ccs_workspace/qc15_main/main.c
+++ ccs_workspace/qc15_main/main.c
@@ -8,171 +8,44 @@
 
 #include "util.h"
 
-#include "lcd111.h"
-#include "ht16d35b.h"
-#include "s25flash.h"
-#include "ipc.h"
-
-volatile uint64_t csecs_of_queercon = 0;
-volatile uint8_t f_time_loop = 0;
-
-void init_clocks() {
-
-    // CLOCK SOURCES
-    // =============
-
-    // Fixed sources:
-    //      VLO      10k Very low power low-frequency oscillator
-    //      MODOSC   5M  for MODCLK
-    //      LFMODCLK (MODCLK/128, 39 kHz)
-
-    // Configurable sources:
-    //      DCO  (Digitally-controlled oscillator) (16 MHz)
-
-    CS_setDCOFreq(1, 4); // Set DCO to 16 MHz
-
-    //      LFXT (Low frequency external crystal) - unused
-    //      HFXT (High frequency external crystal) - unused
-
-    // SYSTEM CLOCKS
-    // =============
-
-    // MCLK (8 MHz)
-    //  Defaults to DCOCLK /8
-    //  Available sources are HFXT, DCO, LFXT, VLO, or external digital clock.
-    //   If it's above 8 MHz, we need to configure FRAM wait-states.
-    //   Set to 8 MHz (DCO /2)
-    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_2); // 8 M
-
-    // SMCLK (1 MHz)
-    //  Defaults to DCOCLK /8
-    //  Same sources available as MCLK.
-    //      NB: This is different from the SMCLK behavior of the FR2xxx series,
-    //          which can only source SMCLK from a divided MCLK.
-    //  We'll use DCO /16
-    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_16); // 1 M
-
-    // MODCLK (5 MHz)
-    //  This comes from MODOSC. It's fixed.
-
-    // ACLK
-    //  Uses LFMODOSC, which is ~ 39k (MODCLK /128).
-    CS_initClockSignal(CS_ACLK, CS_LFMODOSC_SELECT, CS_CLOCK_DIVIDER_1); // 39k
-}
-
-void init_ipc_io() {
-    // IPC:
-    // 2.0 A0_TX
-    // 2.1 A0_RX
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN0+GPIO_PIN1, GPIO_PRIMARY_MODULE_FUNCTION);
-    UCA0CTLW0 |= UCSWRST;
-}
-
-void init_io() {
-    // The magic make-it-work command.
-    //  Otherwise everything is stuck in high-impedance forever.
-    PMM_unlockLPM5();
-
-    lcd111_init_io();
-    ht16d_init_io();
-    s25flash_init_io();
-    init_ipc_io();
-
-    // Screw post inputs with pull-ups
-    P7DIR &= ~(GPIO_PIN2+GPIO_PIN3+GPIO_PIN4); // inputs
-    P7REN |= GPIO_PIN2+GPIO_PIN3+GPIO_PIN4; // resistor enable
-    P7OUT |= GPIO_PIN2+GPIO_PIN3+GPIO_PIN4; // pull-up resistor
-
-    // Buttons: 9.4, 9.5, 9.6, 9.7
-    P9DIR &= ~(BIT4+BIT5+BIT6+BIT7); // inputs
-    P9REN |= (BIT4+BIT5+BIT6+BIT7); // 0xf0
-    P9OUT |= 0xf0; // pull up, please.
-}
-
-/// Initialize the centisecond timer.
-void timer_init() {
-    // We need timer A3 for our loop below.
-    Timer_A_initUpModeParam timer_param = {0};
-    timer_param.clockSource = TIMER_A_CLOCKSOURCE_SMCLK; // 1 MHz
-    // We want this to go every 10 ms, so at 100 Hz (every 10,000 ticks @ 1MHz)
-    //  (a centisecond clock!)
-    timer_param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1; // /1
-    timer_param.timerPeriod = 10000;
-    timer_param.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
-    timer_param.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
-    timer_param.timerClear = TIMER_A_SKIP_CLEAR;
-    timer_param.startTimer = false;
-    Timer_A_initUpMode(TIMER_A1_BASE, &timer_param);
-    Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
-}
-
-void init() {
-    WDT_A_hold(WDT_A_BASE);
-
-    init_clocks();
-
-    init_io();
-
-    lcd111_init();
-    ht16d_init();
-    s25flash_init();
-    ipc_init();
-    timer_init();
-}
-
 void main (void)
 {
-    init();
+    WDT_A_hold(WDT_A_BASE);
+    PMM_unlockLPM5();
 
-    lcd111_text(0, "Test 0");
-    lcd111_text(1, "Test 1");
+    EUSCI_A_UART_initParam uart_param = {0};
 
-    uint8_t rx_from_radio[IPC_MSG_LEN_MAX] = {0};
+    uart_param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK; // 1 MHz
+    uart_param.overSampling = EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION; // 1
+    uart_param.clockPrescalar = 6;
+    uart_param.firstModReg = 8;
+    uart_param.secondModReg = 0x20; // 1/6/8/0x20 = 9600 @ 1 MHz
+    uart_param.parity = EUSCI_A_UART_NO_PARITY;
+    uart_param.msborLsbFirst = EUSCI_A_UART_LSB_FIRST;
+    uart_param.numberofStopBits = EUSCI_A_UART_ONE_STOP_BIT;
+    uart_param.uartMode = EUSCI_A_UART_MODE;
 
-    __bis_SR_register(GIE);
+    EUSCI_A_UART_init(EUSCI_A0_BASE, &uart_param);
+    EUSCI_A_UART_enable(EUSCI_A0_BASE);
+
+    // IPC TX/RX GPIO
+//    P2SEL0 &= ~(BIT0+BIT1);
+//    P2SEL1 &= ~(BIT0+BIT1);
+    // IPC TX/RX Peripheral
+    P2SEL0 |=  (BIT0+BIT1);
+    P2SEL1 &= ~(BIT0+BIT1);
+
+    P2DIR |= BIT0; // UCA0TXD
+    P2DIR &= ~BIT1; // UCA0RXD
+
+    volatile uint8_t rx_in = 0;
+    volatile uint32_t sclk = CS_getSMCLK();
+    __no_operation();
+    volatile uint8_t rx_in_last = 0;
 
     while (1) {
-        if (f_time_loop) {
-            f_time_loop = 0;
-            if (csecs_of_queercon % 200) {
-//                ipc_tx("TEST", 5);
-                EUSCI_A_UART_transmitData(EUSCI_A0_BASE, IPC_SYNC_WORD);
-            }
-        }
-
-        if (f_ipc_rx) {
-            f_ipc_rx = 0;
-            ipc_get_rx(rx_from_radio);
-            rx_from_radio[24]=0;
-            lcd111_text(0, (char *)rx_from_radio);
-        }
-
-        // Go to sleep.
-        LPM0; // TODO: Determine.
+        while (!(UCA0IFG & UCRXIFG)); // wait to RX
+        rx_in = UCA0RXBUF;
+        __no_operation();
     }
-
-    while (1) {
-        led_all_one_color_ring_only(255, 0, 0);
-        delay_millis(250);
-        led_all_one_color_ring_only(255, 8, 0x00);
-        delay_millis(250);
-        led_all_one_color_ring_only(255, 32, 0x00);
-        delay_millis(250);
-        led_all_one_color_ring_only(0, 64, 0);
-        delay_millis(250);
-        led_all_one_color_ring_only(0, 0, 128);
-        delay_millis(250);
-        led_all_one_color_ring_only(128, 0, 128);
-        delay_millis(250);
-    }
-}
-
-// 0xFFDE Timer1_A3 CC0
-#pragma vector=TIMER1_A0_VECTOR
-__interrupt
-void TIMER_ISR() {
-    // All we have here is TA0CCR0 CCIFG0
-    f_time_loop = 1;
-    csecs_of_queercon++;
-    LPM0_EXIT;
 }
--- ccs_workspace/qc15_radiomcu/radio_main.c
+++ ccs_workspace/qc15_radiomcu/radio_main.c
@@ -26,167 +26,56 @@
 #include "driverlib.h"
 #include <msp430fr2422.h>
 
-#include "radio.h"
-#include "ipc.h"
-
-volatile uint8_t f_time_loop = 0;
-volatile uint64_t csecs_of_queercon = 0;
+#define CLOCK_FREQ_KHZ 1000
 
 void delay_millis(unsigned long mils) {
     while (mils) {
-        __delay_cycles(1000);
+        __delay_cycles(CLOCK_FREQ_KHZ);
         mils--;
     }
-}
-
-void init_io() {
-    // The magic FRAM make-it-work command:
-    PMM_unlockLPM5();
-
-    // Radio IO is all set inside the radio driver. For now.
-
-    // Port 1:
-    // -------
-    // * P1.0 RFM CSN (GPIO)    (configured in radio driver)
-    // * P1.1 RFM CLK (Primary) (configured in radio driver)
-    // * P1.2 RFM MO  (Primary) (configured in radio driver)
-    // * P1.3 RFM MI  (Primary) (configured in radio driver)
-    // * P1.4 IPC TX  (Primary)
-    // * P1.5 IPC RX  (Primary)
-    // * P1.6 RFM CE  (GPIO)    (configured in radio driver)
-    // * P1.7 RFM IRQ (GPIO in) (configured in radio driver)
-
-    // IPC TX/RX
-    P1SEL0 |= BIT4+BIT5;
-    P1SEL1 &= ~(BIT4+BIT5); // unneeded but whatever.
-
-    // Port 2:
-    // -------
-    // * P2.0 LFXT (Primary)
-    // * P2.1 LFXT (Primary)
-    // * P2.2 PWSW (GPIO in w/ pull-up) (right/down is HIGH)
-    //
-    //   These are all the usable GPIO pins on the device.
-
-    P2SEL1 = 0b011; // MSB
-    P2SEL0 = 0b000; // LSB
-    P2DIR &= ~BIT2; // Switch pin set to input.
-    P2REN |= BIT2;  // Switch resistor enable
-    P2OUT |= BIT2;  // Switch resistor pull UP direction
-}
-
-void init_clocks() {
-    // CLOCK SOURCES
-    // =============
-
-    // Fixed sources:
-    //      REFO     32k Integrated 32 kHz RC oscillator
-    //      VLO      10k Very low power low-frequency oscillator
-    //      MODOSC   5M  for MODCLK
-
-    // Configurable sources:
-    // DCO  (Digitally-controlled oscillator)
-    //  This defaults to 16 MHz. We'll keep this.
-
-    // LFXT (Low frequency external crystal)
-    CS_turnOnXT1LF(
-            CS_XT1_DRIVE_0
-    );
-
-    CS_initClockSignal(
-            CS_ACLK,
-            CS_XT1CLK_SELECT,
-            CS_CLOCK_DIVIDER_1
-    );
-
-    //clear all OSC fault flag
-    CS_clearAllOscFlagsWithTimeout(1000);
-    // TODO: OSC flags in POST routine, please.
-
-    // SYSTEM CLOCKS
-    // =============
-
-    // MCLK (1 MHz)
-    //  All sources but MODOSC are available at up to /128
-    //  Initializes to DCOCLKDIV (divided DCO) (1 MHz)
-    //  This is fine.
-
-    // SMCLK
-    //  Derived from MCLK with divider up to /8
-    //  Initializes to MCLK/1, which we'll keep.
-
-    // MODCLK (5 MHz)
-    //  This comes from MODOSC
-
-    // ACLK
-    //  Initializes to REFO, which is ~ 32k.
-    //  This is OK, but we'd rather have it connected to our watch crystal,
-    //   which will give us a more precise 32k signal.
-}
-
-void timer_init() {
-    // We need timer A3 for our loop below.
-    Timer_A_initUpModeParam timer_param = {0};
-    timer_param.clockSource = TIMER_A_CLOCKSOURCE_SMCLK; // 1 MHz
-    // We want this to go every 10 ms, so at 100 Hz (every 10,000 ticks @ 1MHz)
-    //  (a centisecond clock!)
-    timer_param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1; // /1
-    timer_param.timerPeriod = 10000;
-    timer_param.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
-    timer_param.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
-    timer_param.timerClear = TIMER_A_SKIP_CLEAR;
-    timer_param.startTimer = false;
-    Timer_A_initUpMode(TIMER_A1_BASE, &timer_param);
-    Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
 }
 
 void main (void)
 {
     //Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
-    init_io();
-    init_clocks();
-    ipc_init();
+    PMM_unlockLPM5();
 
-    // TODO: Wait to load my configuration over IPC-serial.
+    EUSCI_A_UART_initParam uart_param = {0};
 
-    timer_init();
-    radio_init();
+    uart_param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK; // 1 MHz
+    uart_param.overSampling = EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION; // 1
+    uart_param.clockPrescalar = 6;
+    uart_param.firstModReg = 13;
+    uart_param.secondModReg = 0x22; // 1/6/13/0x22 = 9600 @ 1.047576 MHz
+    uart_param.parity = EUSCI_A_UART_NO_PARITY;
+    uart_param.msborLsbFirst = EUSCI_A_UART_LSB_FIRST;
+    uart_param.numberofStopBits = EUSCI_A_UART_ONE_STOP_BIT;
+    uart_param.uartMode = EUSCI_A_UART_MODE;
 
-    uint8_t rx_from_main[IPC_MSG_LEN_MAX] = {0};
+    EUSCI_A_UART_init(EUSCI_A0_BASE, &uart_param);
+    EUSCI_A_UART_enable(EUSCI_A0_BASE);
 
-    __bis_SR_register(GIE);
+//    // IPC TX/RX GPIO
+//    P1SEL0 &= ~(BIT4+BIT5);
+//    P1SEL1 &= ~(BIT4+BIT5);
+//
+//    P1DIR |= BIT4;  // UCA0TXD
+//    P1DIR &= ~BIT5; // UCA0RXD
+
+    P1SEL0 |=  (BIT4+BIT5);
+    P1SEL1 &= ~(BIT4+BIT5);
+
+    volatile uint32_t sclk = CS_getSMCLK();
+    __no_operation();
+
+    volatile uint8_t tx_out = 0;
 
     while (1) {
-        if (f_rfm75_interrupt) {
-            f_rfm75_interrupt = 0;
-            rfm75_deferred_interrupt();
-        }
-
-        if (f_time_loop) {
-            // centisecond.
-            f_time_loop = 0;
-            if (csecs_of_queercon % 100 == 0) {
-                // once per second
-                ipc_tx("Testing IPC", 12);
-            }
-        }
-
-        if (f_ipc_rx) {
-            f_ipc_rx = 0;
-            ipc_get_rx(rx_from_main);
-        }
-
-        __bis_SR_register(LPM0_bits);
+        while (!(UCA0IFG & UCTXIFG)); // wait to TX
+        UCA0TXBUF = tx_out;
+        tx_out++;
+        __no_operation();
+        delay_millis(500);
     }
-}
-
-// 0xFFF4 Timer1_A3 CC0
-#pragma vector=TIMER1_A0_VECTOR
-__interrupt
-void TIMER_ISR() {
-    // All we have here is TA0CCR0 CCIFG0
-    f_time_loop = 1;
-    csecs_of_queercon++;
-    LPM0_EXIT;
 }
