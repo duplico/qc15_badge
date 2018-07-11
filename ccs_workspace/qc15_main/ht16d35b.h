2d5eee8c551dd5a2abfc56ea713f98a4804d2e0a
George Louthan <duplico@dupli.co>
2018-07-10 20:07:37 -0500

    Add function to set & send individual LED sets.

--- ccs_workspace/qc15_main/ht16d35b.h
+++ ccs_workspace/qc15_main/ht16d35b.h
@@ -28,5 +28,6 @@ void ht16d_init();
 void led_send_gray();
 void led_all_one_color(uint8_t r, uint8_t g, uint8_t b);
 void led_all_one_color_ring_only(uint8_t r, uint8_t g, uint8_t b);
+void ht16d_set_colors(uint8_t id_start, uint8_t id_end, rgbcolor16_t* colors);
 
 #endif /* HT16D35B_H_ */
