3934e51bd01f4533d192e9dec2c0170e105425a2
George Louthan <georgerlouth@nthefourth.com>
2018-07-10 21:25:34 -0500

    Get rid of the stupid pad_between idea. If we need this, we can throw flash at it. We have tons of space.

--- ccs_workspace/qc15_main/leds.h
+++ ccs_workspace/qc15_main/leds.h
@@ -34,6 +34,6 @@ typedef struct {
 extern uint8_t s_led_anim_done;
 
 void led_timestep();
-void led_set_anim(led_ring_animation_t *anim, uint8_t anim_type, uint8_t loops, uint8_t pad_between);
+void led_set_anim(led_ring_animation_t *anim, uint8_t anim_type, uint8_t loops);
 
 #endif /* LEDS_H_ */
