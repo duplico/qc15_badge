8df4083fcf1b46656536fd5f7d56688c0fd39fa5
George Louthan <georgerlouth@nthefourth.com>
2018-07-12 01:48:36 -0500

    Move main bootstrap into separate file.

---
+++ ccs_workspace/qc15_main/main_bootstrap.h
@@ -0,0 +1,13 @@
+/*
+ * main_bootstreap.h
+ *
+ *  Created on: Jul 12, 2018
+ *      Author: george
+ */
+
+#ifndef MAIN_BOOTSTRAP_H_
+#define MAIN_BOOTSTRAP_H_
+
+void bootstrap(uint8_t fastboot);
+
+#endif /* MAIN_BOOTSTRAP_H_ */
