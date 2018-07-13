107ee3e09d502dbbb77e0579d080f5d39fddc623
George Louthan <georgerlouth@nthefourth.com>
2018-07-13 00:50:09 -0500

    Add a TOTALLY UNTESTED version of the high-level radio driver. Also TODOs. Oh, the TODOs.

--- ccs_workspace/qc15_common/qc15.h
+++ ccs_workspace/qc15_common/qc15.h
@@ -10,16 +10,32 @@
 
 #define QC15_CRC_SEED 0x5321
 
+// TODO: we need to add 1 to all of these for the null term
+#define QC15_PERSON_NAME_LEN 10
+#define QC15_BADGE_NAME_LEN 10
+
+// TODO:
+// Number of radio flush intervals to keep a badge in the system:
+#define RADIO_GD_INTERVAL 10
+
 // NB: If a base ID higher than QC_BADGES_IN_SYSTEM is needed, then we will
 //     also have to change our radio validator.
 #define QC15_BADGES_IN_SYSTEM 450
 #define QC15_BASE_ID QC15_BADGES_IN_SYSTEM
+#define QC15_HOSTS_IN_SYSTEM 451
 
+#define CODE_SEGMENT_REP_LEN 10
+
+// TODO: Single source of truth in this file, instead of this level of
+//  duplication, please.
 typedef struct {
     uint16_t badge_id;
+    uint8_t person_name[QC15_PERSON_NAME_LEN];
     uint8_t code_segment_ids[6];
-    uint8_t code_segment_unlocks[6][10];
-    uint16_t crc16;
+    uint8_t code_segment_unlocks[6][CODE_SEGMENT_REP_LEN];
+    uint16_t badges_seen_count, badges_connected_count;
+    uint8_t ubers_seen_count, ubers_connected_count;
+    uint8_t handlers_seen_count, handlers_connected_count;
 } qc15status;
 
 typedef struct {
