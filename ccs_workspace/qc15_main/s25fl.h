69c806acdafc0498a5e7a5a3bf1920d838595c14
George Louthan <georgerlouth@nthefourth.com>
2018-07-16 21:44:27 -0500

    Fix overflows in serial flash.

--- ccs_workspace/qc15_main/s25fl.h
+++ ccs_workspace/qc15_main/s25fl.h
@@ -10,10 +10,11 @@
 
 void s25fl_init();
 void s25fl_init_io();
+void s25fl_hold_io();
 uint8_t s25fl_post();
 void s25fl_erase_block_64kb(uint32_t address);
 
 void s25fl_read_data(uint8_t* buffer, uint32_t address, uint32_t len_bytes);
-void s25fl_write_data(uint32_t address, uint8_t* buffer, uint8_t len_bytes);
+void s25fl_write_data(uint32_t address, uint8_t* buffer, uint32_t len_bytes);
 
 #endif /* S25FL_H_ */
