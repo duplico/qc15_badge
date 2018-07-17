82faa22239fb9b8e5dab66687471922817288774
George Louthan <georgerlouth@nthefourth.com>
2018-07-16 23:46:49 -0500

    Implement progress messages EVERY TIME A BASE BEACONS. This is not what we want.

--- ccs_workspace/qc15_radiomcu/radio.c
+++ ccs_workspace/qc15_radiomcu/radio.c
@@ -20,6 +20,23 @@ uint_least8_t ids_in_range[QC15_HOSTS_IN_SYSTEM] = {0};
 /// The current radio packet we're sending (or just sent).
 radio_proto curr_packet_tx;
 
+uint8_t progress_tx_id = 0;
+
+void radio_send_progress_frame(uint8_t frame_id) {
+    radio_progress_payload *payload = (radio_progress_payload *) curr_packet_tx.msg_payload;
+
+    curr_packet_tx.badge_id = badge_status.badge_id;
+    curr_packet_tx.msg_type = RADIO_MSG_TYPE_PROGRESS;
+    curr_packet_tx.proto_version = RADIO_PROTO_VER;
+
+    payload->part_id = badge_status.code_starting_part + frame_id;
+    memcpy(payload->part_data, badge_status.code_part_unlocks[frame_id],
+           CODE_SEGMENT_REP_LEN);
+    crc16_append_buffer(&curr_packet_tx, sizeof(radio_proto)-2);
+
+    rfm75_tx(RFM75_BROADCAST_ADDR, 1, &curr_packet_tx, RFM75_PAYLOAD_SIZE);
+}
+
 uint8_t validate(radio_proto *msg, uint8_t len) {
     if (len != sizeof(radio_proto)) {
         // PROBLEM
@@ -28,7 +45,7 @@ uint8_t validate(radio_proto *msg, uint8_t len) {
     }
 
     // Check for bad ID:
-    if (msg->badge_id > QC15_BADGES_IN_SYSTEM)
+    if (msg->badge_id >= QC15_HOSTS_IN_SYSTEM)
         return 0;
 
     // Finally, verify the CRC:
@@ -37,30 +54,39 @@ uint8_t validate(radio_proto *msg, uint8_t len) {
 
 void radio_handle_beacon(uint16_t id, radio_beacon_payload *payload) {
     // construct an alert to the main badge:
-    // TODO: consider bases
-    ipc_msg_gd_arr_t ipc_out;
-    ipc_out.badge_id = id;
-    memcpy(ipc_out.name, payload->name, QC15_PERSON_NAME_LEN);
 
-    if (ids_in_range[id]) {
-        // Badge was already in range, so now just re-up its flush counter.
-        ids_in_range[id] = RADIO_GD_INTERVAL;
-    } else if (ipc_tx_op_buf(IPC_MSG_GD_ARR, &ipc_out,
-                             sizeof(ipc_msg_gd_arr_t))) {
-        // If it WASN'T already in range, we need to signal that it is now
-        //  in range. But it's very important that this message actually get
-        //  to the main MCU. So we're going to IGNORE the badge if our link
-        //  to the main MCU isn't available, and only treat it as in range if
-        //  we're OK to use the link. It's not very likely that it's
-        //  unavailable, but we still want to avoid having an inconsistency
-        //  between the two processors.
-        // Then again, a foolish consistency is the hobgoblin of little minds,
-        //  adored by little statemen and philosophers and divines. So what the
-        //  heck do I know?
-        ids_in_range[id] = RADIO_GD_INTERVAL;
-        // TODO: If this is a base station:
-        // Let's set a flag that says we need to transmit our STATS!
+    if (id < QC15_BADGES_IN_SYSTEM) {
+        // It's a badge.
+        ipc_msg_gd_arr_t ipc_out;
+        ipc_out.badge_id = id;
+        memcpy(ipc_out.name, payload->name, QC15_PERSON_NAME_LEN);
+
+        if (ids_in_range[id]) {
+            // Badge was already in range, so now just re-up its flush counter.
+            ids_in_range[id] = RADIO_GD_INTERVAL;
+        } else if (ipc_tx_op_buf(IPC_MSG_GD_ARR, &ipc_out,
+                                 sizeof(ipc_msg_gd_arr_t))) {
+            // If it WASN'T already in range, we need to signal that it is now
+            //  in range. But it's very important that this message actually get
+            //  to the main MCU. So we're going to IGNORE the badge if our link
+            //  to the main MCU isn't available, and only treat it as in range if
+            //  we're OK to use the link. It's not very likely that it's
+            //  unavailable, but we still want to avoid having an inconsistency
+            //  between the two processors.
+            // Then again, a foolish consistency is the hobgoblin of little minds,
+            //  adored by little statemen and philosophers and divines. So what the
+            //  heck do I know?
+            ids_in_range[id] = RADIO_GD_INTERVAL;
+        }
+    } else if (id == QC15_BASE_ID) {
+        // It's the suite base
+        // Let's transmit our progress!
+        progress_tx_id = 0;
+        radio_send_progress_frame(0);
+    } else if (id == QC15_CONTROL_ID) {
+        // It's the event controller
     }
+
     // That was easy. The main MCU will handle the rest of the logic. All we
     //  care about is keeping track of who's in range.
 
@@ -121,6 +147,10 @@ void radio_tx_done(uint8_t ack) {
         case RADIO_MSG_TYPE_PROGRESS:
             // We just sent a progress message. Determine whether we need
             //  to send another, or whether that was the last one.
+            if (progress_tx_id == 5) {
+                // This concludes our progress messages.
+                break;
+            }
             // These are unicast messages subject to auto-acknowledgment, but
             //  currently we're not bothering to check the result and resend;
             //  we're just using the auto-ack mechanism to try to add a bit
@@ -128,6 +158,8 @@ void radio_tx_done(uint8_t ack) {
             // If we DO need to send another one, we can just do it from here,
             //  rather than setting a flag; this is because we ARE allowed to
             //  call rfm75_tx() from inside the callback.
+            progress_tx_id++;
+            radio_send_progress_frame(progress_tx_id);
             break;
         case RADIO_MSG_TYPE_STATS:
             // We just sent our STATS somewhere.
@@ -136,21 +168,6 @@ void radio_tx_done(uint8_t ack) {
             //  actually ever going to do it.
             break;
     }
-}
-
-void radio_send_progress_frame(uint8_t frame_id) {
-    radio_progress_payload *payload = (radio_progress_payload *) curr_packet_tx.msg_payload;
-
-    curr_packet_tx.badge_id = badge_status.badge_id;
-    curr_packet_tx.msg_type = RADIO_MSG_TYPE_PROGRESS;
-    curr_packet_tx.proto_version = RADIO_PROTO_VER;
-
-    payload->part_id = badge_status.code_starting_part + frame_id;
-    memcpy(payload->part_data, badge_status.code_part_unlocks[frame_id],
-           CODE_SEGMENT_REP_LEN);
-    crc16_append_buffer(&curr_packet_tx, sizeof(radio_proto)-2);
-
-    rfm75_tx(RFM75_BROADCAST_ADDR, 1, &curr_packet_tx, RFM75_PAYLOAD_SIZE);
 }
 
 /// Do our regular radio and gaydar interval actions.
